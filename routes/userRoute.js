2ca143bacc8d4391d0cba56081a06344221e8740
tarunkumar456 <122202430+tarunkumar456@users.noreply.github.com>
2024-01-08 00:49:01 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,14 @@ const { getNotes, registerUser, loginUser, logout, addNotes, deleteNotes, editNo
 const isaunthenticated = require('../middleware/auth');
 
 const router = express.Router();
+router.get('/', function(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
+    res.send('cors problem fixed:)');
+});
 router.route('/notes').get(isaunthenticated,getNotes);
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
