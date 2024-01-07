472cfac3c97f9df6dec92cef23c46e9f576e99c2
tarunkumar456 <122202430+tarunkumar456@users.noreply.github.com>
2024-01-08 00:20:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const app = express();
 app.use(cookieparser());
 
 app.use(express.json());
-
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 
 const errorMiddleware = require('./middleware/error')
 
