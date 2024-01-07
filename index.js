bad6bc8c3c365df2bdfb991b609e639cd094a417
tarunkumar456 <122202430+tarunkumar456@users.noreply.github.com>
2024-01-08 00:23:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const cors = require('cors');
 //     optionSuccessStatus:200
 // }
 // app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://notes-frontend-zeta.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.get("/", (req, res) => {
     res.json("Hello");
 })
