1144f5489fc2db2373d311422c62311940328c96
tarunkumar456 <122202430+tarunkumar456@users.noreply.github.com>
2024-01-08 22:59:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(
   cors({
     "credentials":true,
-    "origin":"https://notes-frontend-zeta.vercel.app",
+    "origin":"*",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "optionsSuccessStatus": 204
   })
