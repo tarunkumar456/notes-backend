cfd09918e0835060e2cccbac99d15c4bbbdac514
tarunkumar456 <122202430+tarunkumar456@users.noreply.github.com>
2024-01-08 22:36:40 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,7 @@ exports.registerUser = catchasyncerror(async (req, resp) => {
 })
 //login user
 exports.loginUser = catchasyncerror(async (req, resp, next) => {
-     resp.setHeader('Access-Control-Allow-Credentials', true);
+    resp.header('Access-Control-Expose-Headers', "Set-Cookie");
     const { email, password } = req.body;
     //check if both present
     if (!email || !password) {
@@ -131,6 +131,7 @@ exports.getNote = catchasyncerror(async (req, resp, next) => {
 
 //authenticated
 exports.isAuth = catchasyncerror(async (req, resp, next) => {
+    resp.header('Access-Control-Expose-Headers', "Set-Cookie");
     const { token } = req.cookies;
     console.log(req);
     if (!token) {
