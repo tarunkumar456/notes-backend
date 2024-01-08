6625193f4d45928c09a0ae5659b8e94241904c79
tarunkumar456 <122202430+tarunkumar456@users.noreply.github.com>
2024-01-08 20:06:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const cookieparser = require('cookie-parser');
+const cors = require('cors');
 const app = express();
+app.use(
+  cors({
+    "credentials":true,
+    "origin":"*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "optionsSuccessStatus": 204
+  })
+);
 // const fileUpload = require("express-fileupload");
 app.use(cookieparser());
 
